469a57c84fb6b9baf475ed9dd644b20e7b439b3c
Alice5855 <105841246+Alice5855@users.noreply.github.com>
2022-09-23 18:37:33 +0900

    Header fix

--- src/views/Commons/Header.js
+++ src/views/Commons/Header.js
@@ -43,7 +43,7 @@ const Header = () => {
                             <li class="nav-item"><a class="nav-link" href="/QnA">Q&A</a></li>
                             <li class="nav-item"><a class="nav-link" href="/About">About</a></li>
                             <li class="nav-item"><a class="nav-link" href="/Find">Find</a></li>
-                            <li class="nav-item"><a class="nav-link" href="/Contact">Contact</a></li>
+                            <li class="nav-item"><a class="nav-link" href="/FAQ">FAQ</a></li>
                             {isloginCreateBtnLogin()}
                             {islogoutCreateBtnLogout()}
                         </ul>
