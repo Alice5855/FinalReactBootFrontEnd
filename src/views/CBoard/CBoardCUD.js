94c92176eb9fd692fe5e179a2c766ae4f3134b78
Alice5855 <105841246+Alice5855@users.noreply.github.com>
2022-09-27 17:26:04 +0900

    Gotta merge KIM

--- src/views/CBoard/CBoardCUD.js
+++ src/views/CBoard/CBoardCUD.js
@@ -163,7 +163,7 @@ class CBoardCUD extends Component {
             for(var i = 0; i < arr.length; i++){
                 str += "<div>";
                 str += "<img src='/CUpload/display?fileName=" + arr[i].thumbnailURL + "'>";
-                str += "<button class='removeBtn' data-name='" + arr[i].imageURL + "'>REMOVE</button>"
+                str += "<button class='removeBtn btn btn-close ms-2' data-name='" + arr[i].imageURL + "'></button>"
                 str += "<div>"
             }
             divArea.append(str);
